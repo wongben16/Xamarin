1cac569deb2fa368f60fbfbe78ac7abe3c27ae26
Ben <wongben16@gmail.com>
2018-08-01 15:54:23 -0400

    save

---
+++ helloWorld/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace helloWorld
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
